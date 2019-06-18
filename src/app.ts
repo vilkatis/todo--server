71c50ca4a8003601cf7b2e74d2daf6cf8a389c36
victorga <Victor.Garbalau@888holdings.com>
2019-06-18 16:49:36 +0300

    Added error handling to jwt and db

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import { Container } from 'typedi';
 import 'reflect-metadata';
-import { Action, createExpressServer, UnauthorizedError, useContainer } from 'routing-controllers';
+import { Action, createExpressServer, InternalServerError, UnauthorizedError, useContainer } from 'routing-controllers';
 import { Utils } from './helpers';
 import { controllers } from './controllers';
 import { middlewares } from './middlewares';
 import * as jwt from 'jsonwebtoken';
-import { Db, MongoClient } from 'mongodb';
+import { Db, MongoClient, MongoClientOptions } from 'mongodb';
 import { ITokenData } from './models/interfaces';
 
 useContainer(Container);
@@ -15,10 +15,14 @@ Utils.calculateIdealSaltRounds(parseInt(process.env.BCRYPT_SALT_ROUNDS, 10));
 const app = createExpressServer({
   routePrefix: '/v1',
   currentUserChecker: async (action: Action) => {
-    const token: string = action.request.headers['authorization'].split(' ')[1];
-    const secret: string = Utils.getSecret('public_key');
-    const decodedToken: ITokenData = jwt.verify(token, secret, {algorithms: ['RS256']}) as ITokenData;
-    return decodedToken._id;
+    try {
+      const token: string = action.request.headers['authorization'].split(' ')[1];
+      const secret: string = Utils.getSecret('public_key');
+      const decodedToken: ITokenData = jwt.verify(token, secret, {algorithms: ['RS256']}) as ITokenData;
+      return decodedToken._id;
+    } catch(err) {
+      throw new UnauthorizedError('User not authenticated, please log in.')
+    }
   },
   controllers,
   middlewares
@@ -26,10 +30,15 @@ const app = createExpressServer({
 
 app.listen(3000, async () => {
   try {
-    const client: MongoClient = await MongoClient.connect(process.env.DATABASE_URI, {useNewUrlParser: true});
+    const mongoOptions: MongoClientOptions = {
+      useNewUrlParser: true,
+      reconnectInterval: 10000,
+      reconnectTries: 10
+    };
+    const client: MongoClient = await MongoClient.connect(process.env.DATABASE_URI, mongoOptions);
     Container.set(Db, client.db());
     console.log('Connected to DB');
   } catch (err) {
-    console.error('Unable to connect to DB', err);
+    throw new InternalServerError('Unable to connect to DB');
   }
 });
