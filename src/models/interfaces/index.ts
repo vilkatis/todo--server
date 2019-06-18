26125e7dfb2538061b68c1d1a56cf40773bd3b12
victorga <Victor.Garbalau@888holdings.com>
2019-06-18 16:50:32 +0300

    Removed export to previously removed interfaces.

--- src/models/interfaces/index.ts
+++ src/models/interfaces/index.ts
@@ -1,7 +1,5 @@
 export * from './IListDAL';
 export * from './ILoginRequest';
-export * from './IRead';
 export * from './IToken';
 export * from './ITokenData';
 export * from './IUserDAL';
-export * from './IWrite';
